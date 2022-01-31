792345b0cc18d2cc9b70cf1909352a065f47fd42
SChepinog <chepinoga-sv@yandex.ru>
2022-01-31 23:43:51 +0300

    ActiveGame as interface

---
+++ src/main/java/game/active/ActiveBullsCowsGame.java
@@ -0,0 +1,78 @@
+package game.active;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import input.VariationResultSupplier;
+import output.MessageSender;
+
+public class ActiveBullsCowsGame implements ActiveGame {
+    private List<Variation> leftVariations;
+    private int attempts;
+    private boolean isFinished = false;
+    private final VariationResultSupplier resultSupplier;
+    private final MessageSender messageSender;
+    private Supplier<Variation> variationSupplier = () -> VariationChooser.getBestMaxMinChoice(leftVariations);
+
+    public ActiveBullsCowsGame(VariationResultSupplier resultSupplier, MessageSender messageSender) {
+        this.resultSupplier = resultSupplier;
+        this.messageSender = messageSender;
+        this.leftVariations = VariationsUtils.generateAllVariations();
+        this.attempts = 0;
+    }
+
+    public void setVariationSupplier(Supplier<Variation> variationSupplier) {
+        this.variationSupplier = variationSupplier;
+    }
+
+    @Override
+    public void doIteration() {
+        Variation usedVariation = variationSupplier.get();
+        logIterationInput();
+        increaseAttempts();
+        suggestVariation(usedVariation);
+        VariationResult variationResult = resultSupplier.getResult();
+        if (variationWasCorrect(variationResult)) {
+            isFinished = true;
+        } else {
+            updateVariations(usedVariation, variationResult);
+        }
+    }
+
+    private void increaseAttempts() {
+        attempts++;
+    }
+
+    private boolean variationWasCorrect(VariationResult variationResult) {
+        return variationResult.getBulls() == 4;
+    }
+
+    private void updateVariations(Variation usedVariation, VariationResult variationResult) {
+        leftVariations = VariationsUtils.filterVariations(usedVariation, variationResult, leftVariations);
+    }
+
+    private void suggestVariation(Variation testVariation) {
+        messageSender.sendMessage(attempts + " attempt. ");
+        messageSender.sendMessage("Hmm, Is it '" + testVariation.getValue() + "'?");
+    }
+
+    private void logIterationInput() {
+        System.out.println("-------iteration started--------");
+        System.out.println("Left variations: " + leftVariations.size());
+    }
+
+    @Override
+    public void doCongratulations() {
+        messageSender.sendMessage("Hooray! It took " + attempts + " attempts!");
+    }
+
+    @Override
+    public boolean isFinished() {
+        return this.isFinished;
+    }
+
+    @Override
+    public void begin() {
+        messageSender.sendMessage("OK, let's go!");
+    }
+}
